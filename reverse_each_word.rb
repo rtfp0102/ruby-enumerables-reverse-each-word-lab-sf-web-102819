c2b41b1dacf586899082a41e8885f9938a47913d
rtfp0102 <rtfp0102@gmail.com>
2019-10-19 16:45:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence1)
  array = sentence1.split(" ")
- new_array = array.each{|word| word.reverse}
+ new_array = []
+ array.each{|word| new_array.push(word.reverse)}
  new_sentence = new_array.join(" ")
  return new_sentence
 end
