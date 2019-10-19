9c9798818533ea21dddece8dafb3d8d6014eeb9e
rtfp0102 <rtfp0102@gmail.com>
2019-10-19 17:02:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,7 +6,7 @@ def reverse_each_word(sentence1)
  return new_sentence
 end
 def reverse_each_word(sentence1)
- array = sentence1.split(" ")
+ array = %w(sentence1)
  new_array = array.collect{|word| word.reverse}
  new_sentence = new_array.join(" ")
  return new_sentence
