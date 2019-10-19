c172d1ab134057ab9bb5e8764532318d212e7248
rtfp0102 <rtfp0102@gmail.com>
2019-10-19 16:42:21 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+def reverse_each_word(sentence1)
+ array = sentence1.split(" ")
+ new_array = array.each{|word| word.reverse}
+ new_sentence = new_array.join(" ")
+ return new_sentence
+end
+reverse_each_word("Hello there, and how are you?")
